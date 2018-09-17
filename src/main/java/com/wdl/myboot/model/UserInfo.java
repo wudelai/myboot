27148fdae913a45e230f31131367ff9cc5e37d58
wudelai <415587109@qq.com>
2018-09-17 11:18:12 +0800

    test commit code

---
+++ src/main/java/com/wdl/myboot/model/UserInfo.java
@@ -0,0 +1,32 @@
+package com.wdl.myboot.model;
+
+
+public class UserInfo {
+   public String userId;
+   public String userName;
+   public String remark;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
